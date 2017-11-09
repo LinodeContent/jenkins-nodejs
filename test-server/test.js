3acd9c9245d620ab9683b60d6467b264199f5fc6
Damaso Sanoja <damasosanoja@gmail.com>
2017-11-09 14:18:33 -0400

    fixed test.js

--- test-server/test.js
+++ test-server/test.js
@@ -1,7 +1,7 @@
 var supertest = require("supertest");
 var should = require("should");
 
-var server = supertest.agent("http://localhost:9000");
+var server = supertest.agent("http://localhost:express-server");
 
 // Unit Test
 
